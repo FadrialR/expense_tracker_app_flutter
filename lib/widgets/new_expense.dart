f081414d56197f4b9f05f2c32643a54ce1921f2a
Hikmat Fadrial Ramadhan <hikmatfadrial2000@gmail.com>
2024-09-30 20:19:16 +0700

    Creating a fullscreen modal

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -55,6 +55,7 @@ class _NewExpenseState extends State<NewExpense> {
       );
       return; // Make sure no code in the following function is executed
     }
+
     widget.onAddExpense(
       Expense(
         title: _titleController.text,
@@ -63,6 +64,7 @@ class _NewExpenseState extends State<NewExpense> {
         category: _selectedCategory,
       ),
     );
+    Navigator.pop(context);
   }
 
   @override
@@ -75,7 +77,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
